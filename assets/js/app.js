082af8d4b0d922004f1c216e99fc1ab8e332a3d4
rohan panchal <rohanpanchal216@gmail.com>
2023-11-23 01:22:08 +0530

    push

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,11 @@ let cl = console.log;
 ////patch and put=>uppdate
 //////delete
 let card = document.getElementById('card')
+let postformcontrol = document.getElementById('postform');
+let titlecontrol = document.getElementById('title');
+let bodycontrol = document.getElementById('body');
+let useridcontrol = document.getElementById('userid');
+
 
 
 
@@ -14,21 +19,58 @@ let baseurl = `https://jsonplaceholder.typicode.com`
 
 
 
-let xhr = `${baseurl}/posts`
-cl(xhr)
+let posturl = `${baseurl}/posts`
+cl(posturl)
 
 
 ///2>configuration
 
 
-let xhr2 = new XMLHttpRequest();
+// let xhr2 = new XMLHttpRequest();
 
 
-xhr2.open('GET', xhr);
+// xhr2.open('GET', xhr);
 
 
-xhr2.send()
+// xhr2.send()
 
+let postarry = [];
+
+
+
+
+
+const onhandler = (eve) => {
+
+    eve.preventDefault();
+    let newpost = {
+        title: titlecontrol.value,
+        body: bodycontrol.value,
+        userid: useridcontrol.value
+    }
+    cl(newpost);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', posturl, true);
+
+    xhr.send(JSON.stringify(newpost));
+
+    xhr.onload = function () {
+        if (xhr.status === 200 || xhr.status === 201) {
+            newpost.id = JSON.parse(xhr.response).id;
+            postarry.push(newpost);
+    
+            tempalting(postarry);
+            Swal.fire({
+                title: "Good job!",
+                text: "You  have created post",
+                icon: "success"
+              });
+            
+        }
+
+    }
+    postformcontrol.reset()
+}
 
 
 let tempalting = (arr => {
@@ -53,22 +95,33 @@ let tempalting = (arr => {
     });
     card.innerHTML = result;
 })
-xhr2.onload = function gt() {
-    // cl(xhr2.response)
 
-    if (xhr2.status === 200) {
-        let data = JSON.parse(xhr2.response)/// doing parse
-        // cl(data)
-        tempalting(data)
-    } else {
-        alert("something went wrong")
+const gethandler = () => {
+    let xhr2 = new XMLHttpRequest();
+
+
+      xhr2.open('GET', posturl);
+
+
+     xhr2.send()
+    xhr2.onload = function gt() {
+        // cl(xhr2.response)
+
+        if (xhr2.status === 200) {
+            postarry = JSON.parse(xhr2.response)/// doing parse
+            // cl(data)
+            tempalting(postarry)
+        } else {
+            alert("something went wrong")
+        }
+
     }
 
+
 }
+gethandler()
 
-
-
-
+postformcontrol.addEventListener('submit', onhandler)
 
 
 
