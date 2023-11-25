5fbcd97d84fa2da8f67f6bb1548bfc871b02862b
rohan panchal <rohanpanchal216@gmail.com>
2023-11-26 00:43:16 +0530

    push

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,13 @@ let cl = console.log;
 //////delete
 let card = document.getElementById('card')
 let postformcontrol = document.getElementById('postform');
+
 let titlecontrol = document.getElementById('title');
 let bodycontrol = document.getElementById('body');
 let useridcontrol = document.getElementById('userid');
 cl(useridcontrol)
 let updatecontrol = document.getElementById('update');
 let addbtn = document.getElementById('addbtn');
-
 
 
 
@@ -29,7 +29,7 @@ cl(posturl)
 ///2>configuration
 
 
-// let xhr2 = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
 
 // xhr2.open('GET', xhr);
@@ -38,7 +38,20 @@ cl(posturl)
 // xhr2.send()
 
 let postarry = [];
+const onhandler = (eve) => { ///// ist step
 
+    eve.preventDefault();
+    let newpost = {
+        title: titlecontrol.value,
+        body: bodycontrol.value,
+        userid: useridcontrol.value
+    }
+    cl(newpost);//// if onhandler clicked in create obj newpost array pass
+
+    createobj(newpost); // paasing here and taking argumnet upper side
+
+
+}
 const createobj = (postobj) => { //// 2nd step by hitting on add post this will call in 1st step
     let xhr = new XMLHttpRequest();//// creating new instance to send created obj
     xhr.open('POST', posturl, true);////post with posturl // sending to backend
@@ -67,6 +80,7 @@ const createobj = (postobj) => { //// 2nd step by hitting on add post this will 
 const clickhandler = (eve) => {//// edit button functionlity
     // cl(eve)
     let getid = eve.closest('.card').id;/// to get id 
+    localStorage.setItem('get', getid)
     cl(getid);
     let getobjurl = `${baseurl}/posts/${getid}`;//////https://jsonplaceholder.typicode.com/posts/100
     cl(getobjurl)
@@ -90,17 +104,68 @@ const clickhandler = (eve) => {//// edit button functionlity
 
 }
 
-const onhandler = (eve) => { ///// ist step
-
-    eve.preventDefault();
-    let newpost = {
+const postupdate = (ele) => {
+    let updateobj = {
         title: titlecontrol.value,
         body: bodycontrol.value,
-        userid: useridcontrol.value
+        useridcontrol: useridcontrol.value
     }
-    cl(newpost);//// if onhandler clicked in create obj newpost array pass
+    cl(updateobj)
+    let getupdateid = localStorage.getItem('get')
+    cl(getupdateid)
+    let updateurl = `${posturl}/${getupdateid}`
+    cl(updateurl)
 
-    createobj(newpost); // paasing here and taking argumnet upper side
+    let xhr = new XMLHttpRequest();
+    xhr.open('PATCH', updateurl, true)
+    xhr.send(JSON.stringify(updateobj))
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let getid2 = postarry.findIndex(post => {
+
+                return post.id == getupdateid///take one by one array
+            })
+            cl(getid2)///// index number
+            postarry[getid2].title = updateobj.title;/// to update or change keys of crated obj
+            postarry[getid2].body = updateobj.body;
+            postarry[getid2].userid = updateobj.userid;
+
+            tempalting(postarry);
+            Swal.fire({
+                title: "POST UPDATED",
+                text: "things in post are updated",
+                icon: "success"
+            });
+        }
+    }
+
+}
+const deletehandler = (ele) => {
+    cl(ele)
+    let deleteobj = ele.closest('.card').id;
+    cl(deleteobj)
+    let deleteurl = `${posturl}/${deleteobj}`
+    cl(deleteurl)
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', deleteurl, true);
+    xhr.send();
+    xhr.onload = function () {
+        cl(xhr.response)
+        if (xhr.status === 200) {
+            let delete2 = document.getElementById(deleteobj);
+            cl(delete2);
+            delete2.remove();
+            Swal.fire({
+                title: "POST DELETED",
+                text: "things in post are deleted",
+                icon: "success"
+            });
+        }
+
+
+
+    }
+
 
 
 }
@@ -121,7 +186,7 @@ let tempalting = (arr => {
         </div>
         <div class="card-footer  d-flex justify-content-between">
             <button class="btn btn-primary" onclick="clickhandler(this)"> edit</button>
-            <button class="btn btn-danger"> delete</button>
+            <button class="btn btn-danger"onclick="deletehandler(this)"> delete</button>
         </div>
     </div>`
 
@@ -155,6 +220,8 @@ const gethandler = () => { ////for getting data
 gethandler()
 
 postformcontrol.addEventListener('submit', onhandler)
+updatecontrol.addEventListener('click', postupdate)
+
 
 
 
